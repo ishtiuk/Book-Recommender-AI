4ac3f809c727332af78eaaad6576a27e770349d6
Md. Ishtiuk Ahammed <93067876+ishtiuk@users.noreply.github.com>
2023-01-26 23:23:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,14 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/privacypolicy")
+def privacy():
+    return render_template("privacypolicy.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
